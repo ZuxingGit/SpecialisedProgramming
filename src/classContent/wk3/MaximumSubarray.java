ebff29f863fe595db52e79da9c0874a5c5d53025
ZuxingGit <1245766137@qq.com>
2023-08-12 14:01:59 +0930

    MaximumSubarray

---
+++ src/classContent/wk3/MaximumSubarray.java
@@ -0,0 +1,28 @@
+package classContent.wk3;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        int[] nums1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] nums2 = {1};
+        int[] nums3 = {5, 4, -1, 7, 8};
+        int[] nums4 = {-5, 4, -1, -7, -8, -9, -10};
+        System.out.println(maxSubArray(nums1));
+        System.out.println(maxSubArray(nums2));
+        System.out.println(maxSubArray(nums3));
+        System.out.println(maxSubArray(nums4));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+
+        return maxSum;
+    }
+}
