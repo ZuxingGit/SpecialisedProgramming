467d926d4c3b0c0a0a4f589aafe4094b0624418c
ZuxingGit <1245766137@qq.com>
2023-09-08 15:52:12 +0930

    CombinationSum

--- src/classContent/wk7/CombinationSum.java
+++ src/classContent/wk7/CombinationSum.java
@@ -16,8 +16,8 @@ public class CombinationSum {
 //        List<List<Integer>> result2=combinationSum(candidates2,target2);
 //        List<List<Integer>> result3=combinationSum(candidates3,target3);
         System.out.println(combinationSum(candidates1, target1).toString());
-        System.out.println(combinationSum(candidates2,target2).toString());
-        System.out.println(combinationSum(candidates3,target3).toString());
+        System.out.println(combinationSum(candidates2, target2).toString());
+        System.out.println(combinationSum(candidates3, target3).toString());
     }
 
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
@@ -31,7 +31,6 @@ public class CombinationSum {
     }
 
     public static void getCombinations(List<List<Integer>> result, List<Integer> currentList, int[] candidates, int target, int start) {
-//        List<List<Integer>> result = new ArrayList<>();
         if (target < 0) return;
         if (target == 0) {
             result.add(new ArrayList<Integer>(currentList));
@@ -46,6 +45,5 @@ public class CombinationSum {
             getCombinations(result, currentList, candidates, target - candidates[i], i);
             currentList.remove(currentList.size() - 1);
         }
-//        currentList.remove(currentList.size()-1);
     }
 }
