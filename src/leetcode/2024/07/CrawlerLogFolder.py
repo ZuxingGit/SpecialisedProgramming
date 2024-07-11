6cc43f30055153cc98d4c43eae5af7bddfad2de3
ZuxingGit <1245766137@qq.com>
2024-07-11 14:43:39 +0930

    move

--- src/leetcode/2024/07/CrawlerLogFolder.py
+++ src/leetcode/2024/07/CrawlerLogFolder.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/crawler-log-folder/?envType=daily-question&envId=2024-07-10
 from typing import List
+
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
         res = 0
