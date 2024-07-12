baa78d6a71be7fa6648fb6b646aa79518131768b
ZuxingGit <1245766137@qq.com>
2024-07-12 19:00:32 +0930

    DiceOperation

--- src/topcoder/wk2/DiceOperation.java
+++ src/topcoder/wk2/DiceOperation.java
@@ -1,65 +1,59 @@
 package topcoder.wk2;
 
-import java.util.Arrays;
-
 public class DiceOperation {
     public static void main(String[] args) {
-        int[] a1= {1,2,3};
-        getCombinations(a1);
+        int[] a0 = { 1, 1, 1, 1, 1};
+        int[] a1 = { 1, 1, 1, 1, 1, 1};
+        int[] a2 = { 3, 3};
+        int[] a3 = { 3, 4};
+        int[] a4 = { 5, 9, 6};
+        int[] a5 = { 47 };
+        // getCombinations(a1);
+        // System.out.println(getXOR(1, 2));
+        System.out.println(solve(a0));
+        System.out.println(solve(a1));
+        System.out.println(solve(a2));
+        System.out.println(solve(a3));
+        System.out.println(solve(a4));
+        System.out.println(solve(a5));
     }
 
-    public static double solve(int[] dice) {
-        int number = dice.length;   //number of dices
+    public static double solve(int[] dices) {
+        int number = dices.length; // number of dices
         if (number == 0)
             return 0;
-        
+        if (number == 1) {
+            int faces = dices[0];
+            return (faces + 1) / 2;
+        }
+
         double expectV = 0;
-        
-        int total = 1;
-        for (int i = 0; i < number; i++) {
-            total = total * dice[i];    //number of all results
+        int product = 1;
+        for(int n: dices){
+            product *= n;
         }
-        int[][] results= new int[total][number];    //Array of all combinations
-        if (number > 1) {
-            for (int i = 0; i < total; i++) {
-                int[] combine = new int[number];
-                
-                for (int j = 0; j < number; j++) {
-                    int faces = dice[j];    //faces of certain dice
-                    for (int k = 1; k <= faces; k++) {
-                        combine[j]=k;
-                    }
-                }
-                results[i]=combine;
-                
-            }
-            //            
-            
-        } else {
-            
-        }
+
+        double sum = recursiveSum(0, 0, dices);
+        expectV = sum/product;
 
         return expectV;
     }
-    
-    public static int[][] getCombinations(int[] dice){
-        int number = dice.length;
-        int total=1;
-        for (int i = 0; i < number; i++) {
-            total = total * dice[i];    //number of all results
-        }
-        int[][] results= new int[total][number];
-        for (int i = 0; i < number; i++) {
-            int[] combination=new int[number];
-            int value=1;
-            int faces=dice[i];
-            while (i<number-1 && value<faces){
-                
-                value++;
+
+    public static int recursiveSum(int sum0, int index, int[] dices) {
+        int sum1 = 0;
+        int faces = dices[index];
+        for(int i=1; i<= faces; i++) {
+            if (index < dices.length-1) {
+                sum1 += recursiveSum(getXOR(sum0, i), index+1, dices);
+            } else {
+                sum1 += getXOR(sum0, i); 
             }
-            
         }
-        
-        return results;
+
+        return sum1;
+    }
+
+    public static int getXOR(int a, int b) {
+        return a ^ b;
     }
 }
