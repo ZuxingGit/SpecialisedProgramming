8442870cdeea3b12ab1589b0b70d9d9396686ef0
ZuxingGit <1245766137@qq.com>
2024-07-18 09:26:45 +0930

    Max dep of BiTree

---
+++ src/leetcode/2024/07/MaxDepthofBinaryTree.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+sol = Solution()
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+print(sol.maxDepth(root)) # 3
+
+root = TreeNode(1)
+root.right = TreeNode(3)
+print(sol.maxDepth(root)) # 2
