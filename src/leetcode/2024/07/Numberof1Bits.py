e7b65af5fdcc81208fae3b4cc8f1c4d6958ec592
ZuxingGit <1245766137@qq.com>
2024-07-22 16:30:08 +0930

    👈🏻

---
+++ src/leetcode/2024/07/Numberof1Bits.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/number-of-1-bits/description/
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n:
+            n &= n - 1
+            res += 1
+        # while n:
+        #     res += n & 1
+        #     n >>= 1
+        return res
+    
+sol = Solution()
+n = 11
+print(sol.hammingWeight(n)) # 3
+
+n = 128
+print(sol.hammingWeight(n)) # 1
+
+n = 2147483645
+print(sol.hammingWeight(n)) # 30
