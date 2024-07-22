22d2044b6572fb91647c64e1cc1f13f0852b64ba
ZuxingGit <1245766137@qq.com>
2024-07-22 17:50:40 +0930

    update

--- src/leetcode/2024/07/CountingBits.py
+++ src/leetcode/2024/07/CountingBits.py
@@ -2,15 +2,27 @@
 from typing import List
 
 class Solution:
+    # def countBits(self, n: int) -> List[int]:
+    #     res = [0] * (n + 1)
+    #     for i in range(1, n + 1):
+    #         count = 0
+    #         num = i
+    #         while num:
+    #             num &= num - 1
+    #             count += 1
+    #         res[i] = count
+    #     return res
+    
     def countBits(self, n: int) -> List[int]:
         res = [0] * (n + 1)
+        offset = 1
+
         for i in range(1, n + 1):
-            count = 0
-            num = i
-            while num:
-                num &= num - 1
-                count += 1
-            res[i] = count
+            if offset * 2 == i:
+                offset = i
+
+            res[i] = res[i - offset] + 1
+            
         return res
 
 sol = Solution()
