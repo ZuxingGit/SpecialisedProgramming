2f9df04037641f64dc28673c6191ac3dd608c3f3
Zuxing_a1816653 <a1816653@adelaide.edu.au>
2026-02-04 10:51:00 +1030

    add conditions

--- src/leetcode/2026/02/Solution577.sql
+++ src/leetcode/2026/02/Solution577.sql
@@ -1,3 +1,7 @@
+-- Write a solution to report the name and bonus amount of each employee who satisfies either of the following:
+-- The employee has a bonus less than 1000.
+-- The employee did not get any bonus.
+-- Return the result table in any order.
 SELECT e.name, b.bonus
 FROM Employee e
 LEFT JOIN Bonus b ON e.empId = b.empId
