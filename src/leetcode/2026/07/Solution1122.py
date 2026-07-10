9b684a7f6bb6e13e798d3fc58c3ba4d5dd6e47cb
Zuxing_a1816653 <a1816653@adelaide.edu.au>
2026-07-10 22:09:57 +0800

    1122

---
+++ src/leetcode/2026/07/Solution1122.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        """
+        :type arr1: List[int]
+        :type arr2: List[int]
+        :rtype: List[int]
+        """
+        count = {}
+        for num in arr1:
+            count[num] = count.get(num, 0) + 1
+
+        result = []
+        for num in arr2:
+            if num in count:
+                result.extend([num] * count[num])
+                del count[num]
+
+        remaining = sorted(count.keys())
+        for num in remaining:
+            result.extend([num] * count[num])
+
+        return result
